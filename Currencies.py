c7801be1735521f1f06f18825b62a13c33e64e6b
Deepvodolaz <110783453+Deepvodolaz@users.noreply.github.com>
2022-09-12 08:01:18 +0300

    Добавлен файл Currencies.py

--- Currencies.py
+++ Currencies.py
@@ -1,15 +1,14 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-#
-#
-#  
+import requests
+import xmltodict
 
+url = "http://www.cbr.ru/scripts/XML_daily.asp"
+response = requests.get(url)
+data = xmltodict.parse(response.content)
+print (data)
 
-
-def main():
-	
-	return 0
-
-if __name__ == '__main__':
-	main()
-
+my_array =[]
+for item in data['ValCurs']['Valute']:
+    my_set = [item['CharCode'], item['Name'], item['Value']]
+    my_array.append(my_set)
+    print(my_set)
+# ~ print (my_array)    
